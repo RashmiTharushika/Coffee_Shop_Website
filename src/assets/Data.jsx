4e6161c8a504c84eea108a79df695506f1a9ba74
RashmiTharushika <rashmitharushika.info@gmailcom>
2024-07-25 20:44:00 +0530

    Create-header-section

---
+++ src/assets/Data.jsx
@@ -0,0 +1,145 @@
+import menu1 from './img/menu-1.jpg';
+import menu2 from './img/menu-2.jpg';
+import menu3 from './img/menu-3.jpg';
+import menu4 from './img/menu-4.jpg';
+import menu5 from './img/menu-5.jpg';
+import menu6 from './img/menu-6.jpg';
+
+
+import gallery1 from './img/gallery-1.jpg';
+import gallery2 from './img/gallery-2.jpg';
+import gallery3 from './img/gallery-3.jpg';
+import gallery4 from './img/gallery-4.jpg';
+import gallery5 from './img/gallery-5.jpg';
+import gallery6 from './img/gallery-6.jpg';
+
+import testimonial1 from './img/customer-1.jpg';
+import testimonial2 from './img/Customer-2.png';
+
+
+export const links = [
+    {
+      name: 'Home',
+      path: 'home',
+    },
+    {
+      name: 'About',
+      path: 'about',
+    },
+    {
+      name: 'Menu',
+      path: 'menu',
+    },
+    {
+      name: 'Gallery',
+      path: 'gallery',
+    },
+    {
+      name: 'Chefs',
+      path: 'team',
+    },
+    {
+      name: 'Testimonials',
+      path: 'testimonials',
+    },
+  ];
+
+  export const menu = [
+    {
+      id: 1,
+      img: menu1,
+      title: 'Cappuccino',
+      description: 'Milk with vanilla flavored',
+      price: 50.25,
+    },
+    {
+      id: 2,
+      img: menu2,
+      title: 'Black Coffee',
+      description: 'Milk with vanilla flavored',
+      price: 70.25,
+    },
+    {
+      id: 3,
+      img: menu3,
+      title: 'Espresso',
+      description: 'Milk with vanilla flavored',
+      price: 30.25,
+    },
+    {
+      id: 4,
+      img: menu4,
+      title: 'Latte Coffee',
+      description: 'Milk with vanilla flavored',
+      price: 80.25,
+    },
+    {
+      id: 5,
+      img: menu5,
+      title: 'Mocha Coffee',
+      description: 'Milk with vanilla flavored',
+      price: 90.25,
+    },
+    {
+      id: 6,
+      img: menu6,
+      title: 'Flat White Coffee',
+      description: 'Milk with vanilla flavored',
+      price: 70.25,
+    },
+];
+
+export const gallery = [
+    {
+      id: 1,
+      img: gallery1,
+      title: 'Cappuccino',
+    },
+    {
+      id: 2,
+      img: gallery2,
+      title: 'Black Coffee',
+    },
+    {
+      id: 3,
+      img: gallery3,
+      title: 'Espresso',
+    },
+    {
+      id: 4,
+      img: gallery4,
+      title: 'Latte Coffee',
+    },
+    {
+      id: 5,
+      img: gallery5,
+      title: 'Flat White Coffee',
+    },
+    {
+        id: 6,
+        img: gallery6,
+        title: 'Mocha Coffee',
+      },
+  ];
+
+
+
+  export const testimonial = [
+    {
+      id: 1,
+      img: testimonial1,
+      title: 'Luna',
+      service: 'Customer',
+      description:
+        'There are many variations passages of the available but the majority have suffered to the alteration in some form by injected of humour or randomised words.',
+    },
+    {
+      id: 2,
+      img: testimonial2,
+      title: 'Kalix',
+      service: 'Customer',
+      description:
+        'There are many variations passages of the available but the majority have suffered to the alteration in some form by injected of humour or randomised words.',
+    },
+];
+  
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import logo from '../../assets/img/logo.png';
+import {links} from '../../assets/Data';
+import { FaStream } from "react-icons/fa";
 import './header.css';
 
 function Header() {
   return (
-    <div>Header</div>
+
+<header className="header" id="header">
+    <nav className="nav container">
+        <a href="/" className="nav__logo">
+        <img src={logo} alt="Logo" className="nav__logo-img" />
+        </a>
+
+        <div className="nav__menu">
+            <ul className="nav__list">
+            {links.map(({name,path},index) =>{
+                return(
+                    <li className="nav__item" key={index}>
+                         <a href={path} className="nav__link">
+                            {name}
+                        </a>
+                    </li>
+                 );
+})}
+            </ul>
+        </div>
+
+        <div className="nav__toggle">
+          <FaStream />
+        </div>
+    </nav>
+</header>
   )
 }
 

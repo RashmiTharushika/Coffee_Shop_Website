dcdd2a76a2d9a988df7b8b693c7b973a23b57376
RashmiTharushika <rashmitharushika.info@gmailcom>
2024-07-25 22:23:14 +0530

    Create-home-section

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,27 @@ import './home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+<section className="home" id="home">
+    <div className="home__container container">
+        <div className="home__content">
+
+          <h3 className="home__subtitle">
+            Welcome To Brew Haven!
+          </h3>
+          <br />
+
+            <h1 className="home__title">
+            Start Your Day <br />
+            with a Perfect Brew
+            </h1>
+
+            <div className="home__btns">
+                <a href="" className="btn">Buy Now</a>
+            </div>
+
+        </div>
+    </div>
+</section>
   )
 }
 
